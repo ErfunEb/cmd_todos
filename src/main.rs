5ee3e2895ff4db38e80be06baaa113eed2af3475
Erfan Ebrahimi <erfan.ebrahimi@energi.team>
2022-03-31 14:39:26 +0430

    Finishing up, fully functional state app

--- src/main.rs
+++ src/main.rs
@@ -13,18 +13,24 @@ impl App {
     fn menu(&self) {
         println!();
         println!("Choose an option: ");
-        println!("1. list of todos");
+        println!("1. List of todos");
+        println!("2. Add todo");
+        println!("3. Mark todo as done");
+        println!("4. Delete todo");
         println!();
     }
+
     fn input(&self) -> String {
         let mut input = String::new();
         io::stdin()
             .read_line(&mut input)
             .expect("Failed to read a sentence");
+        input.pop();
         input
     }
 
     fn list_todos(&self) {
+        println!("List of todos: ");
         self.connection
             .iterate("SELECT * FROM todos", |pairs| {
                 let mut iteration = pairs.iter();
@@ -33,16 +39,49 @@ impl App {
                 let (_, result) = iteration.next().unwrap();
                 let name = result.unwrap();
                 let (_, result) = iteration.next().unwrap();
-                let done = result.unwrap();
+                let done = if result.unwrap() == "1" {
+                    "true"
+                } else {
+                    "false"
+                };
                 println!("id: {} - name: {} - done: {}", id, name, done);
                 true
             })
             .unwrap();
     }
+
+    fn add_todo(&self) {
+        println!("Enter new todo name: ");
+        let name = self.input();
+        self.connection
+            .execute(format!(
+                "INSERT INTO todos(name, done) VALUES ('{}', {})",
+                name, false
+            ))
+            .expect("Failed to add todo to file");
+    }
+
+    fn mark_as_done(&self) {
+        println!("Please enter todo id that you want to mark as done: ");
+        let id_string = self.input();
+        let id: i32 = id_string.parse().unwrap();
+        self.connection
+            .execute(format!("UPDATE todos SET done={} WHERE id={}", true, id))
+            .expect("Failed to mark todo as done!");
+    }
+
+    fn delete_todo(&self) {
+        println!("Please enter todo id that you want to delete: ");
+        let id_string = self.input();
+        let id: i32 = id_string.parse().unwrap();
+        self.connection
+            .execute(format!("DELETE FROM todos WHERE id={}", id))
+            .expect("Failed to delete todo!");
+    }
 }
 
 fn main() {
-    let connection: Connection = sqlite::open("test.db").unwrap();
+    let connection: Connection = sqlite::open("todos.db").unwrap();
     connection
         .execute(
             "create table if not exists todos (
@@ -56,11 +95,19 @@ fn main() {
 
     loop {
         app.menu();
-        let mut input = app.input();
-        input.pop();
+        let input = app.input();
         match input.as_str() {
             "1" => {
                 app.list_todos();
+            }
+            "2" => {
+                app.add_todo();
+            }
+            "3" => {
+                app.mark_as_done();
+            }
+            "4" => {
+                app.delete_todo();
             }
             _ => {
                 println!("Wrong command!")
